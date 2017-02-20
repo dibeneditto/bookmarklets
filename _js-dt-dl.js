edc476a74eaaba07a2690cd37878873360c304f0
Lukas W. DiBeneditto <lukas@dibeneditto.com>
2017-02-19 21:44:26 -0500

    Create _js-dt-dl.js

---
+++ _js-dt-dl.js
@@ -0,0 +1,12 @@
+/**
+ * Simple JavaScript Bookmarklet to display title and URL.
+ * 
+ * @author Lukas W. DiBeneditto <lukas@dibeneditto.com>
+ * @see {@link http://en.wikipedia.org/wiki/Bookmarklet Bookmarklet - Wikipedia}
+ * @version 1
+ */
+function main() {
+  return document.title + "<br>" + document.URL;
+}
+
+main();
