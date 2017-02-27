11fd0c1c808fc43f78668bc6fe38cff8da17fc86
Lukas W. DiBeneditto <lukas@dibeneditto.com>
2017-02-27 01:23:22 -0500

    Update jsremovepresentation.js

--- jsremovepresentation.js
+++ jsremovepresentation.js
@@ -3,29 +3,30 @@
  * DRAFT This removes most of the presentation of a webpage, but not all.
  * 
  * @author Lukas W. DiBeneditto <lukas@dibeneditto.com>
- * @version 1.3.0
+ * @version 1.4.0
  */
-  function jsRemovePresentation() {
-    var i;
-    /** disable all external and internal stylesheets */
-    for (i = 0; i < document.styleSheets.length; i++) {
-      document.styleSheets[i].disabled = true;
-    } // end for
-    /** replace any elements with the style attribute */
-    var el = document.getElementsByTagName('*');
-    for (i = 0; i < el.length; i++) {
-      el[i].style = '';
-    } // end for
-    /** replace any font elements, getElementsByTagName is
+function jsRemovePresentation() {
+  var i;
+  /** disable all external and internal stylesheets */
+  for (i = 0; i < document.styleSheets.length; i++) {
+    document.styleSheets[i].disabled = true;
+  } // end for
+  /** replace any elements with the style attribute */
+  var el = document.getElementsByTagName('*');
+  for (i = 0; i < el.length; i++) {
+    el[i].style = '';
+  } // end for
+  /** replace any font elements, getElementsByTagName is
      * case-insensitive in HTML */
-    var fo = document.getElementsByTagName('font');
-    for (i = 0; i < fo.length; i++) {
-      var parent = fo[0].parentNode;
-      while (fo[0].firstChild) {
-        parent.insertBefore(fo[0].firstChild, fo[0]);
-      } // end while
-      parent.removeChild(fo[0]);
-    } // end for
-  } // end jsRemovePresentation
+  var fo = document.getElementsByTagName('font');
+  var p;
+  for (i = 0; i < fo.length; i++) {
+    p = fo[0].parentNode;
+    while (fo[0].firstChild) {
+      p.insertBefore(fo[0].firstChild, fo[0]);
+    } // end while
+    p.removeChild(fo[0]);
+  } // end for
+} // end jsRemovePresentation
 
 jsRemovePresentation();
